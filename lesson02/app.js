ec1101df458a329e093f7027fd21b975da09c389
NazimaNurlanova <nazim1art@gmail.com>
2024-03-20 13:45:28 +0300

    commit

--- lesson02/app.js
+++ lesson02/app.js
@@ -1,13 +1,14 @@
 const titleProject = 'оценка стоимости'
 const screensValue = 'шаблонные,с анимациями, с уникальным дизайном'
-const screenPrice = '30900'
-const percentage = '100'
-const fullPrice = '400'
-const responsive = 'true'
+const screenPrice = 30900
+const percentage = 100
+const fullPrice = 400
+const responsive = true
 
 console.log(titleProject);
 console.log(screensValue);
 console.log(screenPrice);
 console.log(percentage);
 console.log(fullPrice);
-console.log(responsive);
+if (responsive) {console.log('okey');} 
+else {console.log('no');}
