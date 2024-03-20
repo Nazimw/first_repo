ba5d32ac5e650d57e82055244674ed868d4d2ad7
NazimaNurlanova <nazim1art@gmail.com>
2024-03-20 13:09:07 +0300

    commit

---
+++ lesson01/app.js
@@ -0,0 +1,5 @@
+let firstName = 'Nazima'
+let age = '29'
+let from = 'istanbul'
+
+console.log(firstName, age, from);
---
+++ lesson02/app.js
@@ -0,0 +1,13 @@
+const titleProject = 'оценка стоимости'
+const screensValue = 'шаблонные,с анимациями, с уникальным дизайном'
+const screenPrice = '30900'
+const percentage = '100'
+const fullPrice = '400'
+const responsive = 'true'
+
+console.log(titleProject);
+console.log(screensValue);
+console.log(screenPrice);
+console.log(percentage);
+console.log(fullPrice);
+console.log(responsive);
